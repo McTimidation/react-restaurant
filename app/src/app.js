8f973b234568e2f6f5b66cf71511b6e435e94fee
McTimidation <michael.mcgraw@student.awesomeinc.org>
2022-10-13 00:18:53 +0000

    header sufficient, not complete

---
+++ app/src/app.js
@@ -0,0 +1,11 @@
+import Header from './header';
+import getData from './getdata';
+
+function App() {
+    getData();
+    return (
+        <Header />
+    )
+}
+
+export default App;
---
+++ app/src/getdata.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+// call api to be stored in STATE. STATE object can be called and updated as user clicks different links
+const pages = { 
+            apps: [],
+            drinks: [],
+            entrees: [],
+            desserts: [],
+            }
+
+
+
+async function getData() {
+    await axios.get('https://astute-baton-362318.ue.r.appspot.com/api/json/')
+        .then((data) => {
+            console.log(data.data[0].category.title)
+        //     data.data.forEach((data) => {
+        //     switch (data.category.title) {
+        //         case "Appetizer":
+        //             pages.apps.push(data)
+        //             break;
+        //         case "Dinner":
+        //             pages.entrees.push(data)
+        //             break;
+        //         case "Dessert":
+        //             pages.desserts.push(data)
+        //             break;
+        //     }
+        // })
+        })
+    
+}
+
+export default getData;
