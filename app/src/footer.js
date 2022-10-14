c280220cf62871ef7b447ed43da03012229093ab
McTimidation <michael.mcgraw@student.awesomeinc.org>
2022-10-14 15:38:36 +0000

    footer looks fine

--- app/src/footer.js
+++ app/src/footer.js
@@ -2,16 +2,25 @@
 
 function Footer() {
     return (
-
-    <footer className={"float-end"}>
-        
-
-
-
-
-
-    </footer>
+<footer>
+    <div class="bg-dark container-fluid">
+      <div class="text-white row">
+        <div class="footer_contact col-lg-4">
+          <h3>Adress</h3>
+          <p>Restaurant 40 Park Ave, Brooklyn <br></br> Ukraine/Nikolaev</p>
+        </div>
+        <div class="footer_contact col-lg-4">
+          <h3>Reservation</h3>
+          <p>+380 12 345 678 91 <br></br> Contact@mail.com</p>
+        </div>
+        <div class="footer_contact col-lg-4">
+          <h3>Open hours</h3>
+          <p>Monday - Friday: 10 AM - 11 <br></br> Saturday - Sunday: 9 AM - 1 PM</p>
+        </div>
+      </div>
+    </div>
+  </footer>
     )
-    }
+}
 
 export default Footer;
