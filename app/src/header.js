02a266a878009bfd726d2743075d27d3ef4157d6
McTimidation <michael.mcgraw@student.awesomeinc.org>
2022-10-14 18:14:22 +0000

    buttons

--- app/src/header.js
+++ app/src/header.js
@@ -1,17 +1,21 @@
 import React from 'react';
 
 
+
+
 function Image() {
     return (
-        <img className="d-block mx-auto mb-4" alt="" src='https://i.imgur.com/tEm9uYV.png'></img>
+        <img className="image" alt="" src='https://i.imgur.com/tEm9uYV.png'></img>
     )
 }
 function Header() {
     return(
-        <div className='bg-dark text-secondary px-4 py-5 text-center'>
-            <Image />
+        <>
+        
+        <div className='image'>
             <div className='display-5 fw-bold text-white'>Revolutionary Pour</div>
         </div>
+       </>
     )
 }
 
--- app/src/menutiles.js
+++ app/src/menutiles.js
@@ -9,7 +9,6 @@ function MenuTiles(props) {
         return (
             <div className='bg-dark text-secondary px-4 py-5 text-center'>
                 <button className="btn btn-outline-light" onClick={() => props.setPage('Appetizers')}>Apps</button>
-                <button className="btn btn-outline-light" >Drinks</button>
                 <button className="btn btn-outline-light" onClick={() => props.setPage('Entrees')}>Entrees</button>
                 <button className="btn btn-outline-light" onClick={() => props.setPage('Desserts')}>Desserts</button>
             </div>
