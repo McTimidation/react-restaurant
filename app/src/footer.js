b62c509770921b74059f653f05990fadaf27e6ab
McTimidation <michael.mcgraw@student.awesomeinc.org>
2022-10-17 00:44:04 +0000

    footerspaced

--- app/src/footer.js
+++ app/src/footer.js
@@ -4,16 +4,16 @@ function Footer() {
     return (
 <footer>
     <div className="bg-dark container-fluid">
-      <div className="text-white row">
-        <div className="footer_contact col-lg-4">
+      <div className="text-white row ">
+        <div className="footer_contact col-lg-4 text-center">
           <h3>Address</h3>
           <p>348 E Main St, Lexington, KY</p>
         </div>
-        <div className="footer_contact col-lg-4">
+        <div className="footer_contact col-lg-4 text-center">
           <h3>Reservation</h3>
-          <p>(502) 741-2817<br></br> Contact@mail.com</p>
+          <p>(502) 741-2817<br></br>RPbar@taste.com </p>
         </div>
-        <div className="footer_contact col-lg-4">
+        <div className="footer_contact col-lg-4 text-center">
           <h3>Open hours</h3>
           <p>Monday - Friday: 10 AM - 11 <br></br> Saturday - Sunday: 9 AM - 1 PM</p>
         </div>
